74364ebbdcf73bc9837e4c5feb14934233287ff7
jlegrand62 <jlegrand62@gmail.com>
2017-05-01 20:33:08 +0200

    Modified 'fusion' to accept a list of initial transformations.
    Useful to pass manual affine deformation in case of failure with the fully
     automatic method (original function).

--- timagetk/algorithms/fusion.py
+++ timagetk/algorithms/fusion.py
@@ -192,3 +192,4 @@ def fusion(list_images, iterations=None, man_trsf_list=None, mean_imgs_prefix=""
         print('Incorrect specifications:')
         print "Specifications log: conds_init={}; conds_list_img={}; conds_list_trsf={}".format(conds_init, conds_list_img, conds_list_trsf)
         return
+==== BASE ====
